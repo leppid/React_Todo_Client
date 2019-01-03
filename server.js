9251a797b8f396aa85582f0cdc7eed61bb7e6d5c
leppid <myvoden@gmail.com>
2019-01-03 14:59:05 +0200

    initial commit

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
+var express = require("express");
+var path = require("path");
 
 app = express();
-app.use(serveStatic(__dirname + "/dist"));
+app.use(express.static(__dirname + "/dist"));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist/index.html"));
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port);
 
-console.log('server started '+ port);
+console.log("server started " + port);
